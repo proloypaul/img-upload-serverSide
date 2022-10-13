9b676a387b5d9c63fd85ca2f60a06ee0590fc618
proloypaul <proloyp23@gmail.com>
2022-10-13 18:40:37 +0600

    require cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e3dsx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
