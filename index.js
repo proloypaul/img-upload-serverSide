62a8f779072943dcb334fa2401c3ac151449367b
proloypaul <proloyp23@gmail.com>
2022-10-13 18:30:56 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e3dsx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+async function run(){
+    try{
+        const database = client.db('ImgUploaderWeb');
+        const usersCollection = database.collection('users');
+
+    }finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("image upload server running");
